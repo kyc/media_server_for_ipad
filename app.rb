9eaf38eac32d0d0d117370232fa821f42d8144ef
kyc <keyongcheng@gmail.com>
2013-05-07 01:11:03 +0800

    fix vtt

--- app.rb
+++ app.rb
@@ -44,7 +44,7 @@ helpers do
         webvtt_lines << "#{srt_line_to_webvtt(line)}"
       end
     else
-      webvtt_lines << "00:00:02.090 --> 02:00:00.000"
+      webvtt_lines << "00:00:02.090 --> 02:00:00.000 vertical:lr align:end\n -"
     end 
     file=File.new(settings.vtt, "w")
     file.write(webvtt_lines.compact.join("\n\n"))
