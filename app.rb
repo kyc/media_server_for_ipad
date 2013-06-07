70b2082deb1c4603b4174e9bbb4a746a4404080c
kyc <keyongcheng@gmail.com>
2013-06-07 17:45:51 +0800

    fix

--- app.rb
+++ app.rb
@@ -33,46 +33,6 @@ class String
   end 
 end
 
-module Unrar
-  class Archive
-    SEARCH_PATH = ["/usr/local/bin", "/usr/bin", "/bin", "/opt/local/bin"]
-
-    attr_accessor :file, :tmpdir
-
-    def initialize(file)
-      self.file = file
-      self.tmpdir = Dir.mktmpdir
-
-      # shitty clean up; fix this
-      at_exit {
-        FileUtils.rm_rf self.tmpdir
-      }
-
-      unless File.readable? file
-        raise "Cannot read #{file}!"
-      end
-    end
-
-    def extract filename, *filenames
-      `#{Archive.unrar} -y x #{self.file} #{filename} #{filenames.join(" ")} #{tmpdir}/`
-
-      Dir["#{tmpdir}/**/*"].to_ary
-    end
-
-    def self.unrar
-      @@unrar ||= search_for "unrar"
-    end
-
-    private
-
-    def self.search_for file
-      SEARCH_PATH.find do |path|
-        return "#{path}/#{file}" if File.exists? "#{path}/#{file}"
-      end
-    end
-  end
-end
-
 helpers do
   
   def media_uri
